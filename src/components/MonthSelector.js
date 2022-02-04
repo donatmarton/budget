1b1a9ba2596e317ce01fd3d7d2f03440034a5648
Márton Donát Nagy <donat@donatmarton.com>
2022-02-04 09:48:23 +0100

    Filtering period can be choosen by user

---
+++ src/components/MonthSelector.js
@@ -0,0 +1,57 @@
+
+const months = Array(12).fill(null).map( (_, index) => {
+  const month = new Date(0, index).toLocaleString('en', { month: 'long' })
+  const obj = {number: index, name: month};
+  return obj
+})
+
+
+const MonthSelector = (props) => {
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = parseInt(event.target.value);
+    props.notifyChange(name, value)
+  }
+
+  return (
+    <div>
+      <form>
+        <select 
+          value={props.month}
+          onChange={handleChange}
+          name="month"
+        >
+          {months.map((month) => {
+            return (
+              <option 
+                key={month.number} 
+                value={month.number}
+              >
+                {month.name}
+              </option>)
+          })}
+        </select>
+        <select 
+          value={props.year}
+          onChange={handleChange}
+          name="year"
+        >
+          <option 
+            key={2021} 
+            value={2021}
+          >
+            2021
+          </option>
+          <option 
+            key={2022} 
+            value={2022}
+          >
+            2022
+          </option>
+        </select>
+      </form>
+    </div>
+  )
+}
+
+export default MonthSelector;
--- src/routes/Overview.js
+++ src/routes/Overview.js
@@ -2,17 +2,39 @@ import React from "react";
 import DataContext from "../DataContext";
 import ExpenseList from "../components/ExpenseList";
 import ExpenseSum from "../components/ExpenseSum";
+import MonthSelector from "../components/MonthSelector";
 
 
 const Overview = () => {
   const { expenses } = React.useContext(DataContext);
+
+  const [filterSelection, setFilterSelection] = React.useState({
+    month: 0,
+    year: (new Date().getFullYear()),
+  })
+  const beginDate = new Date(filterSelection.year, filterSelection.month, 1);
+  const endDate = new Date(filterSelection.year, filterSelection.month, 31);
+
+  const updateFilter = (label, newValue) => {
+    setFilterSelection( prevState => {
+      return {
+        ...prevState,
+        [label]: newValue,
+      }
+    })
+  }
   return (
     <div>
       <h2>Overview</h2>
       <h3>Sum</h3>
+      <MonthSelector 
+        notifyChange={updateFilter} 
+        year={filterSelection.year}
+        month={filterSelection.month}
+      />
       <ExpenseSum 
-        beginDate={new Date(2022, 0, 1)}
-        endDate={new Date(2022, 0, 17)}
+        beginDate={beginDate}
+        endDate={endDate}
       />
       <h3>Expenses</h3>
       <ExpenseList expenses={expenses} />
